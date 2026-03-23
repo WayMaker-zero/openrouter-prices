9a4810993df9167c5fb498f8f5f03f7b8d2ef51e
Habby Labs <way-maker@qq.com>
2026-03-23 14:54:13 +0800

    chore: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/openrouter-prices/',
   plugins: [
     tailwindcss(),
     react()
